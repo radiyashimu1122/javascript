c0c2001842dd09e335a9b6e74075990978b9660f
radiyashimu1122 <radiyashimu@gmail.com>
2023-11-23 03:02:23 -0800

    onlick

---
+++ day-10/practice/test.js
@@ -0,0 +1,16 @@
+function addToCart() {
+ alert ("One item have been added your cart");
+ }
+ function newPage() {
+  open("price=1500tk");
+ }
+ function cencel() {
+  confirm("cencel Your Order");
+ }
+ function payment() {
+  prompt("Inter Your Bkash PIN");
+ }
+ function leaveThisPage() {
+  close();
+ }
+
