f1d5aaa864373e97748196e3e839bb4f92ef3cf0
radiyashimu1122 <radiyashimu@gmail.com>
2023-11-28 08:18:28 -0800

    Removed Previous Commits01~

---
+++ day-10/test.js
@@ -0,0 +1,15 @@
+function addToCart() {
+alert ("One item succsessfully add");
+}
+function chatWith () {
+ confirm("Chat With Daraz");
+}
+function trackNow () {
+ prompt("Track a percel with order id");
+}
+function  Moulvibazar() {
+ open();
+}
+function Hobigang() {
+ close();
+}
---
+++ day-9/test.js
@@ -0,0 +1,4 @@
+const shimuInfo = "shimu"
+console.log(shimuInfo);
+const friendsName =["a","b","c"];
+console.log("friendsName");
